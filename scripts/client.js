99c09e6e5588cbd432aed405b5c397df410c93f5
Trevor Sansalone <trevorksansalone@gmail.com>
2020-10-09 21:56:13 -0500

    adding employees data into stored array

--- scripts/client.js
+++ scripts/client.js
@@ -1,7 +1,28 @@
 console.log('It Works!');
 
+const employeeList = [];
+
 $(document).ready(onReady);
 
 function onReady() {
   console.log('stuff');
+
+  $('.js-click-submit').on('click', clickToSubmit);
+}
+
+function clickToSubmit() {
+  const firstName = $('.js-field-firstName').val();
+  const lastName = $('.js-field-lastName').val();
+  const idNum = $('.js-field-idNum').val();
+  const jobTitle = $('.js-field-jobTitle').val();
+  const salary = $('.js-field-salary').val();
+
+  const employee = {
+    firstName,
+    lastName,
+    idNum,
+    jobTitle,
+    salary,
+  };
+  employeeList.push(employee);
 }
