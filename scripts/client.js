955c8771c2d827834fcd2fd6f0957e809956076a
Trevor Sansalone <trevorksansalone@gmail.com>
2020-10-11 11:16:10 -0500

    taking it back to my base function

--- scripts/client.js
+++ scripts/client.js
@@ -10,12 +10,12 @@ $(document).ready(onReady);
 function onReady() {
   console.log('onReady is ready');
 
-  $('.js-click-submit').on('click', clickToSubmit);
-  $('.js-employee-list').on('click', '.js-btn-selector', selectEmployee);
+  $('.js-click-submit').on('click', addEmployee);
+  $('.js-employee-list').on('click', '.js-btn-delete', deleteEmployee);
 }
 
 // selectors with object for sending employee info
-function clickToSubmit() {
+function addEmployee() {
   const firstName = $('.js-field-firstName').val();
   const lastName = $('.js-field-lastName').val();
   const idNum = $('.js-field-idNum').val();
@@ -28,35 +28,83 @@ function clickToSubmit() {
     idNum,
     jobTitle,
     salary,
+    isDeleted: false,
   };
   employeeList.push(employee);
-  render();
+  displayEmployee();
+  $('.js-field-firstName').val('');
+  $('.js-field-lastName').val('');
+  $('.js-field-idNum').val('');
+  $('.js-field-jobTitle').val('');
+  $('.js-field-salary').val('');
 }
 
-function selectEmployee() {
-  console.log(selectEmployee);
+function deleteEmployee() {
+  const index = $(this).data('index');
+  employeeList[index].isDeleted = true;
+
   $(this)
     .parent() // td
     .parent() // tr
-    .addClass('isSelected');
+    .empty('isDeleted');
 }
 
 // employee table scaffolding, and a template literal
-function render() {
+function displayEmployee() {
   $('.js-employee-list').empty();
+  let monthTotal = 0;
   for (let i = 0; i < employeeList.length; i++) {
-    console.log('render', employeeList[i]);
     const item = employeeList[i];
 
-    $('.js-employee-list').append(
-      `<tr>
-        <td>${item.firstName}</td>
-        <td>${item.lastName}</td>
-        <td>${item.idNum}</td>
-        <td>${item.jobTitle}</td>
-        <td>${item.salary}</td>
-        <td><button class="js-btn-selector">Submit</button></td>
-       </tr>`
-    );
+    if (item.isDeleted === true) {
+    } else
+      $('.js-employee-list').append(
+        `<tr>
+       <td>${item.firstName}</td>
+       <td>${item.lastName}</td>
+       <td>${item.idNum}</td>
+       <td>${item.jobTitle}</td>
+       <td>${item.salary}</td>  
+       <td><button class="js-btn-delete" data-index="${i}">Delete</button></td>
+      </tr>`
+      );
   }
 }
+
+function totalSalaryCost() {
+  let totalSalary = 0;
+  for (let i = 0; i < employeeList.length; i++) {
+    const item = employeeList[i];
+    totalSalary += parseInt(item.annualSalary / 12);
+  }
+  let monthlySalary = totalSalary.toFixed();
+  $('.js-total-salary').text(monthlySalary);
+}
+/*
+function deleteEmployee() {
+  const index = $(this).data('index');
+  displayEmployee(index).isDeleted = true;
+
+  $(this).parent().parent().empty();
+  console.log('working?');
+
+  adjustSalary();
+}
+*/
+/*
+function adjustSalary() {
+  let monthlySalary = 0;
+  for (let i = 0; i < employeeList.length; i++) {
+    const item = employeeList[i];
+  }
+  if (item.isDeleted === false) {
+    monthlySalary += parseInt(item.annualSalary / 12);
+  }
+  console.log(monthlySalary);
+  $('.js-total-salary').text(monthlySalary);
+  if (monthlySalary > 20000)
+    $('.js-total-salary').css('background-color', 'red');
+  else if (monthlySalary < 20000)
+    $('.js-total-salary').css('background-color', 'white');
+}
+*/
